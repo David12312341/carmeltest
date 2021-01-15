ecc9dc7823a0ca52f5a088c06a438cd51723f4b8
David12312341 <45516759+David12312341@users.noreply.github.com>
2021-01-16 00:33:05 +0200

    Add files via upload

---
+++ createDB.py
@@ -0,0 +1,182 @@
+import psycopg2
+import os
+
+def createHallDB(conn):
+	conn.execute("DROP TABLE IF EXISTS res_hall CASCADE;")
+	conn.execute("""
+		CREATE TABLE res_hall(
+			id				 serial UNIQUE,
+			name			 varchar(50),
+			
+		PRIMARY KEY (name)
+		);""")
+
+def createScheduleDB(conn):
+	conn.execute("DROP TABLE IF EXISTS schedule CASCADE;")
+	conn.execute("""
+		CREATE TABLE schedule(
+			id				serial UNIQUE,
+			hall_id			int,
+			month_id		int,
+			created			date,
+
+		PRIMARY KEY (id),
+		FOREIGN KEY (hall_id) REFERENCES res_hall(id),
+		FOREIGN KEY (month_id) REFERENCES month(id)
+		);""")
+
+def createRaDB(conn):
+	conn.execute("DROP TABLE IF EXISTS ra CASCADE;")
+	conn.execute("""
+		CREATE TABLE ra(
+			id				serial UNIQUE,
+			first_name		varchar(20),
+			last_name		varchar(50),
+			hall_id			int,
+			date_started	date,
+			color			varchar(7),
+			email			varchar(256) UNIQUE,
+			auth_level		int DEFAULT 1,
+
+			PRIMARY KEY (id, hall_id),
+			FOREIGN KEY (hall_id) REFERENCES res_hall(id)
+		);""")
+
+def createConflictDB(conn):
+	conn.execute("DROP TABLE IF EXISTS conflicts CASCADE;")
+	conn.execute("""
+		CREATE TABLE conflicts(
+			id		serial UNIQUE,
+			ra_id	int,
+			day_id	int,
+
+			PRIMARY KEY (ra_id, day_id),
+			FOREIGN KEY (ra_id) REFERENCES ra(id),
+			FOREIGN KEY (day_id) REFERENCES day(id)
+		);""")
+
+def createDutyDB(conn):
+	conn.execute("DROP TABLE IF EXISTS duties CASCADE;")
+
+	conn.execute("""
+		CREATE TABLE duties(
+			id			serial UNIQUE,
+			hall_id		int,
+			ra_id		int,
+			day_id		int,
+			sched_id	int,
+			point_val	int DEFAULT 0 CONSTRAINT pos_duty_point_value CHECK (point_val >= 0),
+
+			PRIMARY KEY (id),
+			FOREIGN KEY (hall_id) REFERENCES res_hall(id),
+			FOREIGN KEY (day_id) REFERENCES day(id),
+			FOREIGN KEY (ra_id) REFERENCES ra(id),
+			FOREIGN KEY (sched_id) REFERENCES schedule(id)
+		);""")
+
+def createDayDB(conn):
+	conn.execute("DROP TABLE IF EXISTS day CASCADE;")
+
+	conn.execute("""
+		CREATE TABLE day(
+			id			serial UNIQUE,
+			month_id	int,
+			date		date,
+
+			PRIMARY KEY (month_id,date),
+			FOREIGN KEY (month_id) REFERENCES month(id)
+		);""")
+
+def createMonthDB(conn):
+	conn.execute("DROP TABLE IF EXISTS month CASCADE;")
+
+	conn.execute("""
+		CREATE TABLE month(
+			id			serial UNIQUE,
+			num			int,
+			name		varchar(10),
+			year		date,
+
+			PRIMARY KEY (name,year)
+		);""")
+
+def createUserDB(conn):
+	conn.execute('DROP TABLE IF EXISTS "user" CASCADE;')
+
+	conn.execute("""
+		CREATE TABLE "user"(
+			id			serial UNIQUE,
+			ra_id		int UNIQUE,
+			username	varchar(50) UNIQUE,
+
+			PRIMARY KEY (id),
+			FOREIGN KEY (ra_id) REFERENCES ra(id)
+		);""")
+
+def createOAuthDB(conn):
+	conn.execute('DROP TABLE IF EXISTS flask_dance_oauth CASCADE;')
+
+	conn.execute("""
+		CREATE TABLE flask_dance_oauth(
+			id					serial UNIQUE,
+			provider			varchar(50),
+			created_at			timestamp without time zone,
+			token				json,
+			provider_user_id	varchar(256),
+			user_id				int,
+
+			PRIMARY KEY (id)
+		);""")
+
+def createBreakDutiesTable(conn):
+	conn.execute('DROP TABLE IF EXISTS break_duties CASCADE;')
+
+	conn.execute("""
+		CREATE TABLE break_duties(
+			id			serial UNIQUE,
+			ra_id		int,
+			hall_id		int,
+			month_id	int,
+			day_id		int,
+			point_val	int DEFAULT 0 CONSTRAINT pos_break_duty_point_value CHECK (point_val >= 0),
+
+			PRIMARY KEY (hall_id, month_id, day_id, ra_id),
+			FOREIGN KEY (ra_id) 	REFERENCES ra(id),
+			FOREIGN KEY (hall_id) 	REFERENCES res_hall(id),
+			FOREIGN KEY (month_id) 	REFERENCES month(id),
+			FOREIGN KEY (day_id) 	REFERENCES day(id)
+		);""")
+
+
+def createGoogleCalendarDB(conn):
+	conn.execute("DROP TABLE IF EXISTS google_calendar_info CASCADE;")
+	conn.execute("""
+		CREATE TABLE google_calendar_info(
+			id				serial UNIQUE.
+			res_hall_id		int,
+			auth_state		varchar(30),
+			token 			bytea,
+			calendar_id		varchar(60),
+
+			PRIMARY KEY (res_hall_id),
+			FOREIGN KEY (res_hall_id) REFERENCES res_hall(id)
+		);""")
+
+def main():
+	conn = psycopg2.connect(os.environ["DATABASE_URL"])
+	createHallDB(conn.cursor())
+	createRaDB(conn.cursor())
+	createMonthDB(conn.cursor())
+	createDayDB(conn.cursor())
+	createConflictDB(conn.cursor())
+	createScheduleDB(conn.cursor())
+	createDutyDB(conn.cursor())
+	createBreakDutiesTable(conn.cursor())
+	createUserDB(conn.cursor())
+	createOAuthDB(conn.cursor())
+	createGoogleCalendarDB(conn.cursor())
+
+	conn.commit()
+
+if __name__ == "__main__":
+	main()
---
+++ migrateDB.py
@@ -0,0 +1,76 @@
+from logging.config import dictConfig
+import psycopg2
+import logging
+import os
+
+def migrate(conn):
+    cur = conn.cursor()
+
+    # Check to see if the google_calendar_info table exists
+    cur.execute("SELECT EXISTS (SELECT 1 FROM pg_tables WHERE tablename = 'google_calendar_info');")
+
+    exists = cur.fetchone()
+
+    logging.info("  'google_calendar_info' Table Exists: {}".format(exists))
+
+    if not exists[0]:
+        # If the table does not exist, create the table
+
+        logging.info("  Creating 'google_calendar_info' Table.")
+
+        # Create the google_calendar_info table
+        cur.execute("""
+            CREATE TABLE google_calendar_info(
+                id				serial UNIQUE,
+                res_hall_id		int,
+                auth_state		varchar(30),
+                token 			bytea,
+                calendar_id		varchar(60),
+    
+                PRIMARY KEY (res_hall_id),
+                FOREIGN KEY (res_hall_id) REFERENCES res_hall(id)
+            );""")
+
+    # Drop all Google related columns in the res_hall table
+
+    logging.info("  Removing Google Related Columns From 'res_hall' Table.")
+
+    cur.execute("""
+        ALTER TABLE res_hall
+        DROP COLUMN IF EXISTS calendar_id,
+        DROP COLUMN IF EXISTS g_cal_token,
+        DROP COLUMN IF EXISTS g_cal_auth_state
+    """)
+
+
+
+
+if __name__ == "__main__":
+
+    conn = psycopg2.connect(os.environ["DATABASE_URL"])
+
+    dictConfig({
+        'version': 1,  # logging module specific-- DO NOT CHANGE
+        'formatters': {'default': {
+            'format': '[%(asctime)s.%(msecs)d] %(levelname)s in %(module)s: %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S',
+        }},
+        'handlers': {'wsgi': {
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://flask.logging.wsgi_errors_stream',
+            'formatter': 'default'
+        }},
+        'root': {
+            'level': os.environ["LOG_LEVEL"],
+            'handlers': ['wsgi']
+        }
+    })
+
+    logging.info("Beginning Migration")
+    migrate(conn)
+
+    logging.info("Committing Changes")
+    conn.commit()
+    logging.info("  Finished Committing Changes")
+
+    logging.info("Finished Migration")
